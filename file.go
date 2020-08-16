c07d0580a1673201757fcfe3d78ec8e9eead08b1
Marc Chisinevski <mchisine@redhat.com>
2020-08-15 19:16:05 -0700

    Create file.go

---
+++ file.go
@@ -0,0 +1,32 @@
+package main
+import "fmt"
+import "os"
+import "io"
+// Contents returns the file's contents as a string.
+func Contents(filename string) (string, error) {
+    f, err := os.Open(filename)
+    if err != nil {
+        return "", err
+    }
+    defer f.Close()  // f.Close will run when we're finished.
+
+    var result []byte
+    buf := make([]byte, 100)
+    for {
+        n, err := f.Read(buf[0:])
+        result = append(result, buf[0:n]...) // append is discussed later.
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+            return "", err  // f will be closed if we return here.
+        }
+    }
+    return string(result), nil // f will be closed if we return here.
+}
+
+
+func main() {
+        fileContent, _ := Contents("/home/marc/test.txt")
+	fmt.Printf("%s", fileContent)
+}
